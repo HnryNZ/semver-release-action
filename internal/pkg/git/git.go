883a93a76e5cd5e297fffed491e57da8d21c3649
jude-hnry <jude@hnry.io>
2026-05-11 10:13:39 +1200

    Upgrade Go version and update dependencies

--- internal/pkg/git/git.go
+++ internal/pkg/git/git.go
@@ -7,7 +7,7 @@ import (
 
 	"github.com/K-Phoen/semver-release-action/internal/pkg/action"
 	"github.com/blang/semver/v4"
-	"github.com/google/go-github/v45/github"
+	"github.com/google/go-github/v85/github"
 	"github.com/spf13/cobra"
 	"golang.org/x/oauth2"
 )
